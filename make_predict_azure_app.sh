dc12c6f9f6a967d7b5b4211c85af372a1f10eb90
syedahmad75 <syedahmad75@hotmail.com>
2022-08-22 12:50:00 +0000

    make predict file updated

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://ahmadtestudacity.azurewebsites.net:$PORT/predict 
+     -X POST https://flaskapp-udacity.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
