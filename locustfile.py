e3dd7d19671215247a5179244ae341c05cbc554e
Syed Ahmad <63304762+Syedahmad75@users.noreply.github.com>
2022-08-27 02:20:56 +0200

    Create locustfile.py

---
+++ locustfile.py
@@ -0,0 +1,32 @@
+from locust import HttpUser, between, task
+
+class WebsiteUser(HttpUser):
+    host = "https://flaskapp-udacity.azurewebsites.net:443"
+    wait_time = between(5, 15)
+
+    @task
+    def index(self):
+        self.client.get("/")
+
+    @task
+    def predict(self):
+        self.client.post("/predict", json={
+            "CHAS":{
+                "0":0
+            },
+            "RM":{
+                "0":6.575
+            },
+            "TAX":{
+                "0":296.0
+            },
+            "PTRATIO":{
+                "0":15.3
+            },
+            "B":{
+                "0":396.9
+            },
+            "LSTAT":{
+                "0":4.98
+            }
+        })
