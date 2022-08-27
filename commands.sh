0bac3eacfa92a963f62ece1000abc31ba7cbf27e
Syed Ahmad <63304762+Syedahmad75@users.noreply.github.com>
2022-08-27 02:23:34 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,5 @@
 #Clone the Code into your Azure Cloud Shell Environement
 git clone git@github.com:Syedahmad75/Udacity-Bosch-CICD.git
 cd Udacity-Bosch-CICD
+source ~/.Udacity-Bosch-CICD/bin/activate
+make all
